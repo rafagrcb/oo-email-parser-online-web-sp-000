77b46c3dbea67929f442ef2020757c99fdd9f702
Rafael Garcia <rafagrcb@gmail.com>
2020-07-08 17:11:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@ class EmailAddressParser
   end
 
   def parse
-    email_array = @email.split(/[,]/).uniq
+    email_array = @email.split(/[, ]/).uniq
     email_array.reject! {|element| element.empty?}
     email_array
   end
